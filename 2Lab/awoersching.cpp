3526aebd474a4d77528fbe26d317f569f17fc805
Alex Woersching <ajw006@gmail.com>
2020-07-01 08:01:24 -0700

    Slight modifications made

--- 2Lab/awoersching.cpp
+++ 2Lab/awoersching.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-void merge(int arr[], int begin, int middle, int end)
-void mergeSort(int arr[], int a, int b)
-
 using namespace std;
+
+void merge(int arr[], int begin, int middle, int end);
+void mergeSort(int arr[], int a, int b);
 
 int main(){
     int i, length;
     cin >> length; // Asks user how long the array will be
     int arr [length];
     for(i = 0; i < length; i++) cin >> arr[i]; // Creates array from inputs
-    mergeSort(arr, 0, length); 
+    mergeSort(arr, 0, length-1); 
     for(i = 0; i < length; i++) cout << arr[i] << ";"; // Prints sorted array
     cout << endl;
     return 0;
@@ -19,23 +19,25 @@ int main(){
 void merge(int arr[], int begin, int middle, int end){
     int i, j = 0, k = 0, len = middle - begin + 1, gth = end - middle;
     int first[len], last[gth];
-    for(i = begin; i < middle; i++) first[i] = arr[i]; // Watch out for these lines
-    for(i = middle; i < end; i++) last[i] = arr[i]; // Watch out for these lines
+    for(i = 0; i < middle; i++) first[i] = arr[i]; // Watch out for these lines
+    for(i = 0; i < end; i++) last[i] = arr[i]; // Watch out for these lines
     for(i = begin; i < end; i++){
         if(first[j] < last[k]){
-            arr[i] = first[j++]; // Watch out for these lines
+            arr[i] = first[j]; // Watch out for these lines
+            j++;
         }
         else{
-            arr[i] = last[k++]; // Watch out for these lines
+            arr[i] = last[k]; // Watch out for these lines
+            k++;
         }
     }
 }
 
 void mergeSort(int arr[], int a, int b){
-    if(a < b - 1){
+    if(a < b){
         int middle = (a+b)/2;
         mergeSort(arr, a, middle);
-        mergeSort(arr, middle + 1, b);
+        mergeSort(arr, middle + 1, b-1);
         merge(arr, a, middle, b);
     }
 }
