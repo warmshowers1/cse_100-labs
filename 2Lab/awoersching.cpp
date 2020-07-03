a96966aaa53b669d3fd18a7eccff9d538d4644cd
Alex Woersching <ajw006@gmail.com>
2020-07-02 23:07:23 -0700

    Creates a proper implementation of merge sort
    The merge() function was originally having harsh indexing issues where
    the function was accessing values outside the scope of the arrays it was
    meant to develop on. This has been fixed. The merge() function also makes
    sure to check if we've already sorted through one of the halves passed
    into the function.
    Fixes a problem in the mergeSort() function which was originally
    accounting for the fact that the end of array index parameter, b, is
    meant to represent the final index, rather than the length of the entire
    array. The difference is one off, but would confuse the algorithm
    regardless.
    Adds citation of the source I learned about this algorithm.

--- 2Lab/awoersching.cpp
+++ 2Lab/awoersching.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+/* The implementation of this algorithm was made possible soully 
+ * through the use of the textbook: ISBN: 978-0-262-03384-8
+ */
+
 void merge(int arr[], int begin, int middle, int end);
 void mergeSort(int arr[], int a, int b);
 
@@ -12,24 +16,21 @@ int main(){
     for(i = 0; i < length; i++) cin >> arr[i]; // Creates array from inputs
     mergeSort(arr, 0, length-1); 
     for(i = 0; i < length; i++) cout << arr[i] << ";"; // Prints sorted array
-    cout << endl;
     return 0;
 }
 
 void merge(int arr[], int begin, int middle, int end){
     int i, j = 0, k = 0, len = middle - begin + 1, gth = end - middle;
     int first[len], last[gth];
-    for(i = 0; i < middle; i++) first[i] = arr[i]; // Watch out for these lines
-    for(i = 0; i < end; i++) last[i] = arr[i]; // Watch out for these lines
-    for(i = begin; i < end; i++){
-        if(first[j] < last[k]){
-            arr[i] = first[j]; // Watch out for these lines
-            j++;
-        }
-        else{
-            arr[i] = last[k]; // Watch out for these lines
-            k++;
-        }
+    for(i = 0; i < len; i++) first[i] = arr[begin + i]; // Watch out for these lines
+    for(i = 0; i < gth; i++) last[i] = arr[i + middle + 1]; // Watch out for these lines
+    for(i = begin; i <= end; i++){
+	if(j < len && k < gth){
+            if(first[j] < last[k]) arr[i] = first[j++]; // Watch out for these lines
+            else arr[i] = last[k++]; // Watch out for these lines
+	}
+	else if(j == len) arr[i] = last[k++];
+	else arr[i] = first[j++];
     }
 }
 
@@ -37,7 +38,7 @@ void mergeSort(int arr[], int a, int b){
     if(a < b){
         int middle = (a+b)/2;
         mergeSort(arr, a, middle);
-        mergeSort(arr, middle + 1, b-1);
+        mergeSort(arr, middle + 1, b);
         merge(arr, a, middle, b);
     }
 }
