6e68519d4c208b02883b98873d037d727783866e
Alex Woersching <ajw006@gmail.com>
2020-07-05 15:36:20 -0700

    First implementation of max subarray problem
    Adds the first implementation to find the max subarray in an array of
    integers.
    maxCrossSub() finds the subarray within the array arr[] that both
    passes the midpoint of the array, and also has the largest sum of all
    the numbers in the subarray.
    maxSub() takes the subarray on the left and right halves of the array
    arr[] and then recursively calls itself on those halves, as well as
    calling maxCrossSub() in the process in order to eventually find the
    subarray with the largest sum.

--- 3Lab/awoersching.cpp
+++ 3Lab/awoersching.cpp
@@ -2,7 +2,54 @@
 
 using namespace std;
 
-int main(){
+int* maxCrossSub(int arr[], int a, int mid, int b);
+int* maxSub(int arr[], int a, int b);
 
+int main(){
+    int i, input;
+    cin >> input;
+    int arr[input];
+    for(i = 0; i < input; i++) cin >> arr[i];
+    // int params[3] = maxSub(arr, 0, input);
+    // cout << params[2];
+    cout << maxSub(arr, 0, input)[2];
     return 0;
+}
+
+int* maxCrossSub(int arr[], int a, int mid, int b){
+    // sub[3] = {maxLeft, maxRight, sumLeft + sumRight};  Remember this.
+    int i, sumLeft = NULL, sumRight = NULL, sum = 0;
+    int sub[3] = {mid, mid + 1, NULL};
+    for(i = mid; i > a; i--){
+        sum += arr[i];
+        if(sum > sumLeft || sumLeft == NULL){
+            sumLeft = sum;
+            sub[0] = i;
+        }
+    }
+    sum = 0;
+    for(i = mid + 1; i < b; i++){
+        sum += arr[i];
+        if(sum > sumRight || sumRight == NULL){
+            sumRight = sum;
+            sub[1] = i;
+        }
+    }
+    sub[2] = sumLeft + sumRight;
+    return sub;
+}
+
+int* maxSub(int arr[], int a, int b){
+    if(a == b){ 
+        int soln[3] = {a, b, arr[a]};
+        return soln;
+    }
+    int mid = (a+b)/2;
+    int left[3] = maxSub(arr, a, mid), center[3] = maxCrossSub(arr, a, mid, b), right[3] = maxSub(arr, mid + 1, b);
+    // left = {leftLow, leftHigh, leftSum}
+    // center = {centerLow, centerHigh, centerSum}
+    // right = {rightLow, rightHigh, rightSum}
+    if(left[2] > center[2] && left[2] > right[2]) return left;
+    else if(center[2] > right[2] && center[2] > left[2]) return center;
+    else return right;
 }
