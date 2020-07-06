7a2a5ab840783b2d5b66b71cd17552a19761a8b0
Alex Woersching <ajw006@gmail.com>
2020-07-05 17:33:59 -0700

    Modifies 3Lab code to function properly
    Changes the maxSub() and maxCrossSub() methods in awoersching.cpp to
    instead return an integer value of the sum of the subarray that's being
    analyzed. This change was made after reading the lab instructions more
    carefully and realizing that there was no need to save the bounds of
    these subarrays. This made a lot of code unnecessary, so many changes
    were made to adjust to the new return type.
    Also attempted to edit the .gitignore to remove the a.exe and result
    files from my commits.

--- 3Lab/awoersching.cpp
+++ 3Lab/awoersching.cpp
@@ -2,54 +2,41 @@
 
 using namespace std;
 
-int* maxCrossSub(int arr[], int a, int mid, int b);
-int* maxSub(int arr[], int a, int b);
+// The implementation of this algorithm was made possible soully 
+// through the use of the textbook: ISBN: 978-0-262-03384-8
+// on pages 68 - 73
+
+int maxCrossSub(int arr[], int a, int mid, int b);
+int maxSub(int arr[], int a, int b);
 
 int main(){
     int i, input;
     cin >> input;
     int arr[input];
     for(i = 0; i < input; i++) cin >> arr[i];
-    // int params[3] = maxSub(arr, 0, input);
-    // cout << params[2];
-    cout << maxSub(arr, 0, input)[2];
+    cout << maxSub(arr, 0, input - 1);
     return 0;
 }
 
-int* maxCrossSub(int arr[], int a, int mid, int b){
-    // sub[3] = {maxLeft, maxRight, sumLeft + sumRight};  Remember this.
-    int i, sumLeft = NULL, sumRight = NULL, sum = 0;
-    int sub[3] = {mid, mid + 1, NULL};
+int maxCrossSub(int arr[], int a, int mid, int b){
+    int i, sub, sumLeft = arr[mid], sumRight = arr[mid + 1], sum = 0;
     for(i = mid; i > a; i--){
         sum += arr[i];
-        if(sum > sumLeft || sumLeft == NULL){
-            sumLeft = sum;
-            sub[0] = i;
-        }
+        if(sum > sumLeft) sumLeft = sum;
     }
     sum = 0;
-    for(i = mid + 1; i < b; i++){
+    for(i = mid + 1; i <= b; i++){
         sum += arr[i];
-        if(sum > sumRight || sumRight == NULL){
-            sumRight = sum;
-            sub[1] = i;
-        }
+        if(sum > sumRight) sumRight = sum;
     }
-    sub[2] = sumLeft + sumRight;
+    sub = sumLeft + sumRight;
     return sub;
 }
 
-int* maxSub(int arr[], int a, int b){
-    if(a == b){ 
-        int soln[3] = {a, b, arr[a]};
-        return soln;
-    }
-    int mid = (a+b)/2;
-    int left[3] = maxSub(arr, a, mid), center[3] = maxCrossSub(arr, a, mid, b), right[3] = maxSub(arr, mid + 1, b);
-    // left = {leftLow, leftHigh, leftSum}
-    // center = {centerLow, centerHigh, centerSum}
-    // right = {rightLow, rightHigh, rightSum}
-    if(left[2] > center[2] && left[2] > right[2]) return left;
-    else if(center[2] > right[2] && center[2] > left[2]) return center;
+int maxSub(int arr[], int a, int b){
+    if(a == b) return arr[a];
+    int left = maxSub(arr, a, (a+b)/2), center = maxCrossSub(arr, a, (a+b)/2, b), right = maxSub(arr, ((a+b)/2) + 1, b);
+    if(left > center && left > right) return left;
+    else if(center > right && center > left) return center;
     else return right;
 }
