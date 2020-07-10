623179e02770fcb69614bb6701cc0ef88e5cf459
Alex Woersching <ajw006@gmail.com>
2020-07-10 13:38:56 -0700

    Creates functioning heap sort algorithm
    Debugs code to create a properly functioning heap sort algorithm.
    Fixes if statements in maxHeap() that was causing the method to make
    comparisons that incorrectly sorted the array.
    Fixes heapSort() function by changing the starting index of the second
    for loop. This was causing the for loop to start outside of the bounds
    of the array. Also changes the size parameter when calling maxHeap()
    because the heap size is meant to decrease as heapSort() continues.

--- 4Lab-1/awoersching.cpp
+++ 4Lab-1/awoersching.cpp
@@ -8,42 +8,43 @@ using namespace std;
 
 void maxHeap(int arr[], int size, int x);
 void heapSort(int arr[], int size);
-void swap(int* x, int* y);
 int left(int i);
 int right(int i);
 int parent(int i);
+void swap(int* x, int* y);
 
 int main(){
     int i, input;
     cin >> input;
     int arr[input];
     for(i = 0; i < input; i++) cin >> arr[i];
-    
+    heapSort(arr, input);
+    for(i = 0; i < input; i++) cout << arr[i] << ";";
     return 0;
 }
 
 void maxHeap(int arr[], int size, int x){
     int big;
-    if(left(x) <= size && arr[left(x)] > arr[right(x)]) big = left(x);
+    if(left(x) < size && arr[left(x)] > arr[x]) big = left(x);
     else big = x;
-    if(right(x) <= size && arr[right(x)] > arr[big]) big = right(x);
+    if(right(x) < size && arr[right(x)] > arr[big]) big = right(x);
     if(big != x){
         swap(arr[x], arr[big]);
-        max-heap(arr, size, big);
+        maxHeap(arr, size, big);
     }
 }
 
 void heapSort(int arr[], int size){
     int i;
     for(i = size/2; i >= 0; i--) maxHeap(arr, size, i); // Builds max heap
-    for(i = size; i > 0; i--){
+    for(i = size - 1; i > 0; i--){
         swap(arr[0], arr[i]);
-        maxHeap(arr, size, 0);
+        maxHeap(arr, --size, 0);
     }
 }
-int left(int i) return 2*i;
-int right(int i) return 2*i + 1;
-int parent(int i) return i/2;
+int left(int i){ return i * 2;}
+int right(int i){ return (i * 2) + 1;}
+int parent(int i){ return i / 2;}
 
 void swap(int* x, int* y){
     if(*x != *y){
