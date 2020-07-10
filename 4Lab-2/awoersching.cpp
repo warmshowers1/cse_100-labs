52b0299772732a20f53d0e1ceb3d8ab9a1b52ec6
Alex Woersching <ajw006@gmail.com>
2020-07-10 09:33:26 -0700

    Implements quick sort algorithm
    Creates the quick sort algorithm, has NOT been debugged yet.
    randoSplit() is meant to randomly partition the array and return the
    index where the array is being partitioned.
    randoQuick() is meant to be the main recursive sorting the algorithm.
    part() is meant to partition the array by choosing the pivot point,
    which has been randomized by randoSplit() and then sorting the array
    from the beginning to the pivot point based one whether or not an
    element is smaller than the element at the pivot point.
    swap() is simply meant to swap two elements in an array.

--- 4Lab-2/awoersching.cpp
+++ 4Lab-2/awoersching.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 int main(){
-    
+    int i, input;
+    cin >> input;
+    int arr[input];
+    for(i = 0; i < input; i++) cin >> arr[i];
+    randoQuick(arr, 0, input-1);
     return 0;
+}
+
+int randoSplit(int arr[], int a, int b){
+    int r = rand() % (b-a) + a;
+    /*
+    int hold = arr[r];
+    arr[r] = arr[b];
+    arr[b] = hold;
+    */
+    swap(arr[r], arr[b]);
+    return part(arr, a, b);
+}
+
+void randoQuick(int arr[], int a, int b){
+    if(a < b){
+        int r = randoSplit(arr, a, b);
+        randoQuick(arr, a, half-1);
+        randoQuick(arr, half+1, b);
+    }
+}
+
+int part(int arr[], int a, int b){
+    int i, x = a - 1;
+    for(i = 0; i <= b-1; i++)
+        if(arr[i] <= arr[b]) swap(arr[++x], arr[i]);
+    swap(arr[++x], arr[b]);
+    return ++x;
+}
+
+void swap(int* x, int* y){
+    if(*x != *y){
+        int hold = *x;
+        *x = *y;
+        *y = hold;
+    }
 }
