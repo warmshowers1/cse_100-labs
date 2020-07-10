684fdc44fc0910f6e3653e78c9ce850543d2c812
Alex Woersching <ajw006@gmail.com>
2020-07-10 10:07:32 -0700

    Debugged quick sort
    Added a line in main() to print the final sorted array.
    Fixed an incorrect variable error in randoQuick().
    Fixed part() to return the correct index, rather than increasing the
    index variable x by one more before returning (causing seg faults).

--- 4Lab-2/awoersching.cpp
+++ 4Lab-2/awoersching.cpp
@@ -5,22 +5,23 @@
 
 using namespace std;
 
+int randoSplit(int arr[], int a, int b);
+void randoQuick(int arr[], int a, int b);
+int part(int arr[], int a, int b);
+void swap(int* x, int* y);
+
 int main(){
     int i, input;
     cin >> input;
     int arr[input];
     for(i = 0; i < input; i++) cin >> arr[i];
     randoQuick(arr, 0, input-1);
+    for(i = 0; i < input; i++) cout << arr[i] << ";";
     return 0;
 }
 
 int randoSplit(int arr[], int a, int b){
     int r = rand() % (b-a) + a;
-    /*
-    int hold = arr[r];
-    arr[r] = arr[b];
-    arr[b] = hold;
-    */
     swap(arr[r], arr[b]);
     return part(arr, a, b);
 }
@@ -28,17 +29,17 @@ int randoSplit(int arr[], int a, int b){
 void randoQuick(int arr[], int a, int b){
     if(a < b){
         int r = randoSplit(arr, a, b);
-        randoQuick(arr, a, half-1);
-        randoQuick(arr, half+1, b);
+        randoQuick(arr, a, r-1);
+        randoQuick(arr, r+1, b);
     }
 }
 
 int part(int arr[], int a, int b){
     int i, x = a - 1;
-    for(i = 0; i <= b-1; i++)
+    for(i = a; i < b; i++)
         if(arr[i] <= arr[b]) swap(arr[++x], arr[i]);
     swap(arr[++x], arr[b]);
-    return ++x;
+    return x;
 }
 
 void swap(int* x, int* y){
