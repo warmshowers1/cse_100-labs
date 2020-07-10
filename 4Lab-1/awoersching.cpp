f46325c0306d5e005f1ef18270540bdf30b5c212
Alex Woersching <ajw006@gmail.com>
2020-07-10 11:15:44 -0700

    Creates method to make array into max heap array
    While creating the heap sort algorithm, it's necessary to make sure the
    array is sorted in a manner that would suggest that it has max heap
    properties. Thus the max-heap() method is created.
    The left(), right(), and parent() methods are simple helper methods
    meant to return the index of the left, right, and parent node of a
    binary tree respectively.
    swap() is a helper function taken from the quick sort algorithm that
    switches any two elements in an array.

--- 4Lab-1/awoersching.cpp
+++ 4Lab-1/awoersching.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 
+// The implementation of this algorithm was made possible soully 
+// through the use of the textbook: ISBN: 978-0-262-03384-8
+// on pages 151 - 161
+
 using namespace std;
 
+int left(int i);
+int right(int i);
+int parent(int i);
+
 int main(){
+    int i, input;
+    cin >> input;
+    int arr[input];
+    for(i = 0; i < input; i++) cin >> arr[i];
     
     return 0;
+}
+
+void max-heap(int arr[], int size, int x){
+    int big;
+    if(left(x) <= size && arr[left(x)] > arr[right(x)]) big = left(x);
+    else big = x;
+    if(right(x) <= size && arr[right(x)] > arr[big]) big = right(x);
+    if(big != x){
+        swap(arr[x], arr[big]);
+        max-heap(arr, size, big);
+    }
+}
+
+int left(int i) return 2*i;
+int right(int i) return 2*i + 1;
+int parent(int i) return i/2;
+
+void swap(int* x, int* y){
+    if(*x != *y){
+        int hold = *x;
+        *x = *y;
+        *y = hold;
+    }
 }
