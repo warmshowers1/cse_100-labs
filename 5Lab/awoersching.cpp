9798baf0e38f04d024a3f96217fc4e71759d2736
Alex Woersching <ajw006@gmail.com>
2020-07-15 14:17:05 -0700

    Allows the input to be stored properly
    Using the test cases given in testfiles, these unique inputs had to be
    properly stored. The code given in main() now does this, as well as
    implements the code to print the sorted output of radSort().

--- 5Lab/awoersching.cpp
+++ 5Lab/awoersching.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main(){
-    
+    int i, j, length, num;
+    cin >> length;
+    vector<int> arr[length];
+    for(i = 0; i < length; i++){
+        while(arr[i].size() < 10){
+            cin >> num;
+            if(num >= 0 && num <= 3) arr[i].push_back(num);
+            else arr[i].push_back(-1);
+        }
+    }
+    radSort(arr, 10);
+    for(i = 0; i < length; i++)
+        for(j = 0; j < 10; j++)
+            cout << arr[i][j] << ";" << endl;
     return 0;
+}
+
+void radSort(vector<int> arr[], int digit){
+    for(int i = 0; i < digit; i++){
+        
+    }
 }
