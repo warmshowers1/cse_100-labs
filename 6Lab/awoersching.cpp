62f263d06663267fbfe4d4449928afbfb755da45
Alex Woersching <ajw006@gmail.com>
2020-07-17 17:07:59 -0700

    Implements functioning hash table
    Debugs helper functions.
    Creates new helper function strToInt() which takes in a string and
    returns an integer. Created to help call the functions in main().
    main() now asks the user for input and parses it to check which command
    the user wants to do. For example, the command "i2" will insert the
    number 2 into the correct bucket of the hash table. "d2" will delete
    that number from the hash table and output that the value was deleted.
    "o" will simply output the entire table, and "s2" will search for the
    first instance of the number 2 in the hash table and output whether or
    not the value exists and where it exists.
    These commands work for any n digit number.

--- 6Lab/awoersching.cpp
+++ 6Lab/awoersching.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<list>
 #include<string>
+#include<math.h>
 
 // The implementation of this algorithm was made possible 
 // using the textbook: ISBN: 978-0-262-03384-8
@@ -12,10 +13,11 @@
 using namespace std;
 
 int h(int k);
-void i(list<int> arr, int k);
+void i(list<int> arr[], int k);
 void d(list<int> arr[], int k);
 void s(list<int> arr[], int k);
 void o(list<int> arr[]);
+int strToInt(string str);
 
 int m;
 
@@ -23,48 +25,60 @@ int main(){
     cin >> m;
     list<int> arr[m];
     string input = "";
-    while(input != "e" || input != "E"){
+    while(input.front() != 'e' && input.front() != 'E'){
         cin >> input;
-        
+        if(input.front() == 'i' || input.front() == 'I') i(arr, strToInt(input.substr(1, input.length())));
+        else if(input.front() == 'd' || input.front() == 'D') d(arr, strToInt(input.substr(1, input.length())));
+        else if(input.front() == 's' || input.front() == 'S') s(arr, strToInt(input.substr(1, input.length())));
+        else if(input.front() == 'o' || input.front() == 'O') o(arr);
     }
     return 0;
 }
 
-int h(int k){
-    return k % m;
-}
+int h(int k){return k % m;}
 
-void i(list<int> arr[], int k){
-    arr[h(k)].push_front(k);
-}
+void i(list<int> arr[], int k){arr[h(k)].push_front(k);}
 
 void d(list<int> arr[], int k){
     list<int>::iterator i;
-    for(i = arr[h(k)].begin(); i != arr[h(k)].end(); ++i){ // Watch this
-        if(*i == k) arr[h(k)].erase(i);
-        cout << k << ":DELETED;" << endl;
+    bool found = false;
+    for(i = arr[h(k)].begin(); i != arr[h(k)].end(); i++){
+        if(*i == k){ 
+            arr[h(k)].erase(i);
+            cout << k << ":DELETED;" << endl;
+            found = true;
+            break;
+        }
     }
-    if(*i == *arr[h(k)].end()) cout << k << ":DELETE_FAILED;" << endl;
+    if(!found) cout << k << ":DELETE_FAILED;" << endl;
 }
 
 void s(list<int> arr[], int k){
     list<int>::iterator i;
     int j = 0;
-    for(i = arr[h(k)].begin(); i != arr[h(k)].end(); ++i){ // Watch this
+    bool found = false;
+    for(i = arr[h(k)].begin(); i != arr[h(k)].end(); i++){
         if(*i == k){
-            cout << k << ":FOUND_AT" << h(k) << j << ";" << endl;
+            cout << k << ":FOUND_AT" << h(k) << "," << j << ";" << endl;
+            found = true;
             break;
         }
         j++;
     }
-    if(*i == *arr[h(k)].end()) cout << k << ":NOT_FOUND;" << endl;
+    if(!found) cout << k << ":NOT_FOUND;" << endl;
 }
 
 void o(list<int> arr[]){
     list<int>::iterator j;
     for(int i = 0; i < m; i++){
         cout << i << ":";
-        for(j = arr[i].begin(); j != arr[i].end(); ++j) cout << *j << "->";
+        for(j = arr[i].begin(); j != arr[i].end(); j++) cout << *j << "->";
         cout << ";" << endl;
     }
+}
+
+int strToInt(string str){
+    int i, j, result = 0;
+    for(i = str.length() - 1; i >= 0; i--) result += ((((int) str[i]) - 48) * pow(10, str.length() - i - 1));
+    return result;
 }
