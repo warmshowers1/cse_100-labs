3e591e4ae0a4b537ec6b149eac0ed1762faea2ef
Alex Woersching <ajw006@gmail.com>
2020-07-24 09:18:43 -0700

    Created functional rod cutting function
    cutRod() takes in an array of prices and finds the maximum amount of
    revenue that can be made from cutting the rod into spsecific lengths.
    This program still needs to be able to print the lengths this rod needs
    to be cut into to achieve the maximum revenue.

--- 7Lab/awoersching.cpp
+++ 7Lab/awoersching.cpp
@@ -2,11 +2,31 @@
 
 // The implementation of this algorithm was made possible 
 // through the use of the textbook: ISBN: 978-0-262-03384-8
-// on pages ### - ###
+// on pages 360 - 366
 
 using namespace std;
 
+int cutRod(int* p);
+
+int n;
+
 int main(){
-    
+	cin >> n;
+	int i, p[n+1];
+	p[0] = 0; 
+	for(i = 1; i <= n; i++) cin >> p[i];
+    cout << cutRod(p) << endl;
     return 0;
+}
+
+int cutRod(int* p){
+    int i, j, max, r[n+1];
+    r[0] = 0;
+    for(i = 1; i <= n; i++){
+        max = p[0];
+        for(j = 1; j <= i; j++)
+            if(max < (p[j] + r[i - j])) max = (p[j] + r[i - j]);
+        r[i] = max;
+    }
+    return r[n];
 }
--- 7Lab/verifier.cpp
+++ 7Lab/verifier.cpp
@@ -6,23 +6,14 @@ using namespace std;
 int main(int argc, char **argv) {
 
   // Get the size of the sequence
-  int n = 1;
+  int i, n = 1;
   int* p; 
 
   cin >> n;
 
   p = new int[n+1];
-  //read p; 
   p[0] = 0; 
-  for(int i=1; i<=n; i++)
-  {
-    cin >> p[i];  
-  }
-
-
- 
- // for(int i=1; i<=n; i++)
- //   cout << p[i]<< endl;
+  for(i = 1; i <= n; i++) cin >> p[i];
 
   int total_length = 0; 
   int total_revenue = 0; 
@@ -38,7 +29,7 @@ int main(int argc, char **argv) {
       total_length += length;
       total_revenue += p[length]; 
     }
-  }while(length >0);
+  }while(length > 0);
 
   //cout << total_length << endl;
   //cout << total_revenue << endl;
