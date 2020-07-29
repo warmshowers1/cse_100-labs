01c3d09220c5528ec1e50d75e0eb343e30a344b4
Alex Woersching <ajw006@gmail.com>
2020-07-29 04:15:57 -0700

    Properly implements Matrix Chain Multiplication
    Adds a 2D array, s, initialized within matChain() meant to hold the
    optimal indices to split the array, arr, to find the most optimal way
    to multiply each matrix together.
    lookup() was slightly changed to take in this new array, s, and modify
    it properly. This made the printOptimal() method useful.
    matChain() was changed to a void function, as it was redundant to have
    it returning the value we were only meant to print.

--- 8Lab/awoersching.cpp
+++ 8Lab/awoersching.cpp
@@ -6,59 +6,57 @@
 
 using namespace std;
 
-int n;
+void matChain(int arr[]);
+int lookup(int arr[], int** m, int** s, int i, int j);
+void printOptimal(int** s, int i, int j);
 
-int matChain(int arr[]);
-int lookup(int arr[], int** m, int i, int j);
-void printMatrix(int** arr, int p, int q);
-void printOptimal(int* s[], int i, int j);
+int n;
 
 int main(){
     cin >> n;
     int i = 0, arr[n+1];
     while(i <= n) cin >> arr[i++];
-    cout << matChain(arr) << endl;
+    matChain(arr);
     return 0;
 }
 
-int matChain(int arr[]){
+void matChain(int arr[]){
     int i, j;
-    int** m = new int*[n];
-    for(i = 0; i < n; i++) m[i] = new int[n];
+    int **m = new int*[n], **s = new int*[n];
+    for(i = 0; i < n; i++){
+        m[i] = new int[n];
+        s[i] = new int[n];
+    }
     for(i = 0; i < n; i++)
         for(j = i; j < n; j++)
             m[i][j] = -1;
-    return lookup(arr, m, 0, n-1);
+    cout << lookup(arr, m, s, 0, n-1) << endl;
+    printOptimal(s, 0, n-1);
+    cout << endl;
 }
 
-int lookup(int arr[], int** m, int i, int j){
+int lookup(int arr[], int** m, int** s, int i, int j){
     int k, q;
     if(m[i][j] > -1) return m[i][j];
     if(i == j) m[i][j] = 0;
     else{
         for(k = i; k < j; k++){
-            q = lookup(arr, m, i, k) + lookup(arr, m, k+1, j) + (arr[i] * arr[k+1] * arr[j+1]);
-            if((m[i][j] == -1) || (m[i][j] > q)) m[i][j] = q;
+            q = lookup(arr, m, s, i, k) + lookup(arr, m, s, k+1, j) + (arr[i] * arr[k+1] * arr[j+1]);
+            if((m[i][j] == -1) || (m[i][j] > q)){
+                m[i][j] = q;
+                s[i][j] = k;
+            }
         }
     }
     return m[i][j];
 }
 
-void printMatrix(int** arr, int p, int q){
-    for(int i = 0; i < p; i++){
-        for(int j = 0; j < q; j++)
-            cout << arr[i][j] << " ";
-        cout << endl;
-    }
-}
-
-void printOptimal(int* s[], int i, int j){
-   if(i == j) cout << "A" << i; 
-   else{
-       cout << "(";
-       printOptimal(s, i, s[i][j]);
-       printOptimal(s, ++s[i][j], j);
-       cout << ")";
+void printOptimal(int** s, int i, int j){
+    if(i == j) cout << "A" << i; 
+    else{
+        cout << "(";
+        printOptimal(s, i, s[i][j]);
+        printOptimal(s, ++s[i][j], j);
+        cout << ")";
    }
-   cout << endl;
 }
