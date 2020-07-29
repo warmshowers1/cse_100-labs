5d59f4a98cd7463b60b5b9125d1b794185d10471
Alex Woersching <ajw006@gmail.com>
2020-07-29 02:01:11 -0700

    Starts on the course to find optimal matrix mult
    Creates two functions:
    matChain() takes in the array of matrix dimensions, arr[], and the 2D
    array s which will be manipulated by matChain() to hold the optimal
    indices from which we can split arr in order to do the lowest amount of
    operations.
    printMatrix() as it states, it takes in a matrix and its dimensions, and
    prints said matrix.

--- 8Lab/awoersching.cpp
+++ 8Lab/awoersching.cpp
@@ -2,11 +2,48 @@
 
 // The implementation of this algorithm was made possible 
 // through the use of the textbook: ISBN: 978-0-262-03384-8
-// on pages ### - ###
+// on pages 370 - 377
 
 using namespace std;
 
+void matChain(int arr[], int* s[]);
+void printMatrix(int** arr, int p, int q);
+
+int n;
+
 int main(){
-    
+    cin >> n;
+    int i = 0, arr[n+1];
+    while(i <= n) cin >> arr[i++];
+    int* s[n-1];
+    matChain(arr, s);
     return 0;
+}
+
+void matChain(int arr[], int* s[]){
+    int i, j, k, l, q, m[n-1][n-1];
+    for(i = 0; i < (n-1); i++) m[i][i] = 0;
+    // This be where the magic happens
+    for(l = 2; l <= n; l++){
+        for(i = 1; i < (n-l+1); i++){
+            j = i + l - 1;
+            m[i][j] = -1;
+            for(k = i; k < j; k++){
+                q = m[i][k] + m[(k+1)][j] + (arr[i-1] * arr[k] * arr[j]);
+                if(q < m[i][j]){
+                    m[i][j] = q;
+                    s[i][j] = k;
+                }
+            }
+        }
+    }
+    printMatrix(s, n-1, n);
+}
+
+void printMatrix(int** arr, int p, int q){
+    for(int i = 0; i < p; i++){
+        for(int j = 0; j < q; j++)
+            cout << arr[i][j] << " ";
+        cout << endl;
+    }
 }
