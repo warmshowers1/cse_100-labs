37b4048931b65ecdceeabf3575d75485befb1f1f
Alex Woersching <ajw006@gmail.com>
2020-08-02 10:42:11 -0700

    Adds input structure and creates node struct
    Creates a structure named node containing a frequency of a particular
    letter appearing, as well as a left and right pointer to other nodes.
    Edits main() to take in user input of the 6 different frequencies of
    each letter and insert them into the array of nodes.
    Begins work on greed() algorithm which will sort all 6 frequencies using
    the Huffman code algorithm and print the final codes that will be used
    to identify each of the 6 characters.
    Adds citation.
    Note: Due to the lab assignment, the code is going to constantly assume
    there will be exactly 6 characters that will need to be analyzed.

--- 9Lab/awoersching.cpp
+++ 9Lab/awoersching.cpp
@@ -2,11 +2,28 @@
 
 // The implementation of this algorithm was made possible 
 // through the use of the textbook: ISBN: 978-0-262-03384-8
-// on pages ### - ###
+// on pages 428 - 436
 
 using namespace std;
 
+void greed(int arr[]);
+
+struct node {
+    int freq;
+    struct node* left;
+    struct node* right;
+};
+
 int main(){
-    
+    int i;
+    final int n = 6;
+    node arr[n];
+    for(i = 0; i < n; i++) cin >> arr[i].freq;
+    greed(arr, n);
     return 0;
 }
+
+void greed(int arr[], int l){
+    
+}
+
