0c82a73ada9e51a71e479e078bafd05b30d20588
Alex Woersching <ajw006@gmail.com>
2020-08-04 14:24:57 -0700

    Creates vector of Nodes and min() function
    A vector of Nodes has been created to act as a queue. All other methods
    have been modified to reflect this change.
    greed() now takes in the vector q, and enacts the Huffman algorithm onto
    it.
    min() returns the index from which the lowest frequency node is in the
    vector q.
    This code still has issues maintaining the final tree.

--- 9Lab/awoersching.cpp
+++ 9Lab/awoersching.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 
 // The implementation of this algorithm was made possible 
 // through the use of the textbook: ISBN: 978-0-262-03384-8
@@ -6,24 +7,62 @@
 
 using namespace std;
 
-void greed(int arr[]);
+void greed(vector<struct Node> *q);
+int min(vector<struct Node> q);
 
-struct node {
+struct Node {
+    char letter;
     int freq;
-    struct node* left;
-    struct node* right;
+    struct Node* left;
+    struct Node* right;
+
+    Node(){
+        letter = 0;
+        freq = 0;
+        left = NULL;
+        right = NULL;
+    }
+    Node(char a, int b){
+        letter = a;
+        freq = b;
+        left = NULL;
+        right = NULL;
+    }
 };
 
+const int n = 6;
+
 int main(){
-    int i;
-    final int n = 6;
-    node arr[n];
-    for(i = 0; i < n; i++) cin >> arr[i].freq;
-    greed(arr, n);
+    int i, input;
+    vector<struct Node> q;
+    for(i = 0; i < n; i++){
+        cin >> input;
+        q.push_back(Node('A' + i, input));
+    }
+    greed(&q);
     return 0;
 }
 
-void greed(int arr[], int l){
-    
+void greed(vector<struct Node> *q){
+    Node *z; 
+    int i, s = min(*q);
+    vector<struct Node>::iterator it = q->begin();
+    for(i = 0; i < (n-1); i++){
+        z = new Node;
+        z->left = new Node((*q)[s].letter, (*q)[s].freq);
+        (*q).erase(it + s);
+        s = min(*q);
+        z->right = new Node((*q)[s].letter, (*q)[s].freq);
+        (*q).erase(it + s);
+        z->freq = z->left->freq + z->right->freq;
+        (*q).push_back(*z);
+    }
 }
 
+int min(vector<struct Node> q){
+    vector<struct Node>::iterator it = q.begin()++;
+    int i, p = 0;
+    for(i = 1; i < q.size(); i++)
+        if(it++->freq < q[p].freq) p = i;
+    return p;
+}
