b2395e93c7d885e76b41558d171a38a9e408c67e
Alex Woersching <ajw006@gmail.com>
2020-08-05 18:06:42 -0700

    Initializes Lab 10
    Adds all files necessary to begin Lab 10 and begins adding the first
    lines of code.

---
+++ 10Lab/awoersching.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+// The implementation of this algorithm was made possible 
+// through the use of the textbook: ISBN: 978-0-262-03384-8
+// on pages ### - ###
+
+using namespace std;
+
+int v, e;
+
+struct Vert{
+    int num;
+    vector<struct Vert> edges;
+};
+
+int main(){
+    cin >> v;
+    cin >> e;
+    int i;
+    for(i = 0; i < e; i++){
+        
+    }
+    return 0;
+}
