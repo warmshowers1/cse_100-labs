89ee65c9927ee7fb5ec92ba62cab96818edc0825
Alex Woersching <ajw006@gmail.com>
2020-08-05 22:29:11 -0700

    Replaces struct and adds helper functions
    Replaces the struct Vert() with two vector<int> arrays, g[] & t[],
    where t[] is the meant to be the transpose of g[].
    transpose() takes in two vector<int> arrays g[] & t[], and transposes
    g[] by storing it in t[].
    printArray() simply takes in an array and prints the contents of each
    index. Might delete later.

--- 10Lab/awoersching.cpp
+++ 10Lab/awoersching.cpp
@@ -7,19 +7,47 @@
 
 using namespace std;
 
-int v, e;
+void transpose(vector<int> g[], vector<int> t[]);
+void printArray(vector<int> g[]);
 
-struct Vert{
-    int num;
-    vector<struct Vert> edges;
-};
+int v, e;
 
 int main(){
     cin >> v;
+    vector<int> g[v], t[v];
     cin >> e;
-    int i;
+    int i, num, edge;
     for(i = 0; i < e; i++){
-        
+        cin >> num >> edge;
+        // This if statement is primarily an 
+        // idiot check for debugging purposes
+        if(((num < v) && (num >= 0)) && ((edge < v) && (edge >= 0)))
+            g[num].push_back(edge);
+        else{ 
+            cout << "Try again" << endl;
+            --i;
+        }
     }
+    transpose(g, t);
+    printArray(g);
+    printArray(t);
     return 0;
+}
+
+void transpose(vector<int> g[], vector<int> t[]){
+    int i, j;
+    for(i = 0; i < v; i++){
+        for(j = 0; j < g[i].size(); j++)
+            t[g[i][j]].push_back(i);
+    }
+}
+
+void printArray(vector<int> g[]){
+    int i, j;
+    for(i = 0; i < v; i++){
+        cout << i << ":";
+        for(j = 0; j < g[i].size(); j++)
+            cout << g[i][j] << ",";
+        cout << endl;
+    }
 }
