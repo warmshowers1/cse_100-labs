e6a986d3832490ef700fc15e56b0c2d5faf0e666
Alex Woersching <ajw006@gmail.com>
2020-08-06 23:23:59 -0700

    Properly enacts dfs on the transpose of g
    endTimeOrder() returns a list of integers that represent the verticies
    in order of the highest end times to the lowest end times.
    dfsTPose() enacts dfs on the transpose of g. This function is
    specifically meant to go in order of the returned value of
    endTimeOrder().

--- 10Lab/awoersching.cpp
+++ 10Lab/awoersching.cpp
@@ -11,6 +11,8 @@ void transpose(vector<int> g[], vector<int> t[]);
 void printArray(vector<int> g[]);
 int *dfs(vector<int> g[]);
 void dfsv(vector<int> g[], int node, int *time, int start[], int end[], bool visited[]);
+int *endTimeOrder(int normEnds[]);
+int *dfsTPose(vector<int> t[], int foo[]);
 
 int v, e;
 
@@ -30,7 +32,12 @@ int main(){
             --i;
         }
     }
+    transpose(g, t);
     normEnds = dfs(g);
+    int *normEndOrder = endTimeOrder(normEnds);
+    int *bar = dfsTPose(t, normEndOrder);
+    for(i = 0; i < v; i++) cout << bar[i] << ",";
+    cout << endl;
     return 0;
 }
 
@@ -68,15 +75,45 @@ int *dfs(vector<int> g[]){
     return end;
 }
 
-void dfsv(vector<int> g[], int node, int *time, int start[], int end[], bool visited[]){
+void dfsv(vector<int> g[], int vertex, int *time, int start[], int end[], bool visited[]){
     int j, parent;
-    start[node] = ++*(time);
-    visited[node] = true;
-    for(j = 0; j < g[node].size(); j++){
-        if(visited[g[node][j]] == false){
-            parent = node;
-            dfsv(g, g[node][j], time, start, end, visited);
+    start[vertex] = ++*(time);
+    visited[vertex] = true;
+    for(j = 0; j < g[vertex].size(); j++){
+        if(visited[g[vertex][j]] == false){
+            parent = vertex;
+            dfsv(g, g[vertex][j], time, start, end, visited);
         }
     }
-    end[node] = ++*(time);
+    end[vertex] = ++*(time);
+}
+
+int *endTimeOrder(int normEnds[]){
+    int i, j, *foo = (int*)malloc(v * sizeof(int));
+    for(i = 0; i < v; i++){
+        foo[i] = -1;
+        for(j = 0; j < v; j++){
+            if((normEnds[j] > normEnds[foo[i]]) && ((i > 0) && (normEnds[j] < normEnds[foo[i-1]])))
+                foo[i] = j;
+            else if((i == 0) && (normEnds[j] > normEnds[foo[i]]))
+                foo[i] = j;
+        }
+    }
+    return foo;
+}
+
+int *dfsTPose(vector<int> t[], int foo[]){
+    bool visited[v];
+    int i, time = 0, start[v], *end = (int*)malloc(v * sizeof(int));
+    for(i = 0; i < v; i++){
+        visited[i] = false;
+        start[i] = 0;
+        end[i] = 0;
+    }
+    for(i = 0; i < v; i++){
+        if(visited[foo[i]] == false){
+            dfsv(t, foo[i], &time, start, end, visited);
+        }
+    }
+    return end;
 }
