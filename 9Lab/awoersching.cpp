c342759501a0186452fbf84b03acf19e2f4a0f5c
Alex Woersching <ajw006@gmail.com>
2020-08-07 18:17:45 -0700

    Recommits final version of Lab 9

--- 9Lab/awoersching.cpp
+++ 9Lab/awoersching.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<string>
 
 // The implementation of this algorithm was made possible 
 // through the use of the textbook: ISBN: 978-0-262-03384-8
@@ -7,8 +8,9 @@
 
 using namespace std;
 
-void greed(vector<struct Node> *q);
+struct Node* greed(vector<struct Node> *q);
 int min(vector<struct Node> q);
+void findCodes(struct Node *root, string codes[], string code);
 
 struct Node {
     char letter;
@@ -28,41 +30,62 @@ struct Node {
         left = NULL;
         right = NULL;
     }
+    Node(char a, int b, Node *c, Node *d){
+        letter = a;
+        freq = b;
+        left = c;
+        right = d;
+    }
 };
 
 const int n = 6;
 
 int main(){
     int i, input;
+    string codes[n];
     vector<struct Node> q;
     for(i = 0; i < n; i++){
         cin >> input;
         q.push_back(Node('A' + i, input));
     }
-    greed(&q);
+    findCodes(greed(&q), codes, "");
     return 0;
 }
 
-void greed(vector<struct Node> *q){
+struct Node* greed(vector<struct Node> *q){
     Node *z; 
-    int i, s = min(*q);
-    vector<struct Node>::iterator it = q->begin();
+    int i, s;
     for(i = 0; i < (n-1); i++){
         z = new Node;
-        z->left = new Node((*q)[s].letter, (*q)[s].freq);
-        (*q).erase(it + s);
         s = min(*q);
-        z->right = new Node((*q)[s].letter, (*q)[s].freq);
-        (*q).erase(it + s);
+        z->left = new Node((*q)[s].letter, (*q)[s].freq, (*q)[s].left, (*q)[s].right);
+        (*q).erase(q->begin() + s);
+        s = min(*q);
+        z->right = new Node((*q)[s].letter, (*q)[s].freq, (*q)[s].left, (*q)[s].right);
+        (*q).erase(q->begin() + s);
         z->freq = z->left->freq + z->right->freq;
         (*q).push_back(*z);
     }
+    return z;
 }
 
 int min(vector<struct Node> q){
-    vector<struct Node>::iterator it = q.begin()++;
+    vector<struct Node>::iterator it = ++q.begin();
     int i, p = 0;
     for(i = 1; i < q.size(); i++)
         if(it++->freq < q[p].freq) p = i;
     return p;
 }
+
+void findCodes(struct Node* root, string codes[], string code){
+    if(root->letter != 0) codes[(int) (root->letter - 65)] = code;
+    else{
+        findCodes(root->left, codes, code + "0");
+        findCodes(root->right, codes, code + "1");
+    }
+    if(code == ""){
+        for(int i = 0; i < n; i++)
+            cout << (char)('A' + i) << ":" << codes[i] << endl;
+    }
+}
+
