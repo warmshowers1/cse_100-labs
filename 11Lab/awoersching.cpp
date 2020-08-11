cb21fbf4254353a851e6b572b1f7cb4f2e20591e
Alex Woersching <ajw006@gmail.com>
2020-08-10 17:58:39 -0700

    Initializes necessary variables in main()
    Creates matrix of weights meant to represent the weights connected
    between two verticies. If there is no connection between two verticies,
    -1 is placed in the indicies as a place holder. This is assuming there
    will be no negatively weighed edges.
    The graph, G[], will be an array of vectors that hold all the verticies
    connected to any one vertex. So the edges of vertex 1 will be held in
    G[1] and so on.

--- 11Lab/awoersching.cpp
+++ 11Lab/awoersching.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
+#include<vector>
 
 // The implementation of this algorithm was made possible 
 // through the use of the textbook: ISBN: 978-0-262-03384-8
-// on pages ### - ###
+// on pages 634 - 636
 
 using namespace std;
 
+int verts, edges;
+
 int main(){
-    
+    cin >> verts;
+    cin >> edges;
+    int i, j, u, v, w, *parents, weights[verts][verts]; // Weights list
+    vector<int> G[verts]; // Graph
+    for(i = 0; i < verts; i++)
+        for(j = 0; j < verts; j++)
+            weights[i][j] = -1;
+    for(i = 0; i < edges; i++){
+        cin >> u >> v >> w;
+        graph[u].push_back(v);
+        graph[v].push_back(u); //These are here because
+        weights[u][v] = w; 
+        weights[v][u] = w; // graph is undirected
+    }
     return 0;
 }
+
