8b374ccff1998534daad4a7dda1ccd38a695fc84
Alex Woersching <ajw006@gmail.com>
2020-08-10 20:26:16 -0700

    Creates first prim() method, adds minor bug fixes
    Creates general idea of what prim() should do. Meant to create a minimum
    spanning tree and return the parents of every vertex. Still needs to be
    worked on, as the function causes seg faults.
    search() is a simple helper function that checks a vector to see if an
    integer value, v, is present.
    weights[][] was changed to be a double pointer as it is easier to pass
    into the method prim().

--- 11Lab/awoersching.cpp
+++ 11Lab/awoersching.cpp
@@ -7,23 +7,58 @@
 
 using namespace std;
 
+int* prim(vector<int> G[], int **weights);
+bool search(vector<int> Q, int v);
+
 int verts, edges;
 
 int main(){
     cin >> verts;
     cin >> edges;
-    int i, j, u, v, w, *parents, weights[verts][verts]; // Weights list
+    int i, j, u, v, w, *parents;
+    int **weights = (int**)malloc(verts * sizeof(int*));
     vector<int> G[verts]; // Graph
     for(i = 0; i < verts; i++)
+        weights[i] = (int*)malloc(verts * sizeof(int));
         for(j = 0; j < verts; j++)
             weights[i][j] = -1;
     for(i = 0; i < edges; i++){
         cin >> u >> v >> w;
-        graph[u].push_back(v);
-        graph[v].push_back(u); //These are here because
+        G[u].push_back(v);
+        G[v].push_back(u); //These are here because
         weights[u][v] = w; 
         weights[v][u] = w; // graph is undirected
     }
+    parents = prim(G, weights);
     return 0;
 }
 
+int* prim(vector<int> G[], int **weights){
+    int i, u, v, min, num, *parents = (int*)malloc(verts * sizeof(int)), keys[verts];
+    vector<int> Q;
+    for(i = 0; i < verts; i++){
+        parents[i] = -1;
+        keys[i] = -1;
+        Q.push_back(i);
+    }
+    keys[0] = 0;
+    while(Q.empty() == false){
+        u = Q[0];
+        Q.erase(Q.begin());
+        for(i = 0; i < G[u].size(); i++){
+            v = G[u][i];
+            if((search(Q, v)) && ((weights[u][v] < keys[v] || v == -1))){
+                parents[v] = u;
+                keys[v] = weights[u][v];
+            }
+        }
+    }
+    return parents;
+}
+
+bool search(vector<int> Q, int v){
+    for(int i : Q)
+        if(v == i)
+            return true;
+    return false;
+}
