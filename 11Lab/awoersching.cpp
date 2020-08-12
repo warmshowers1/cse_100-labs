e3a88fca8a1a61ea02690dcdd7153aab20985aec
Alex Woersching <ajw006@gmail.com>
2020-08-11 23:44:54 -0700

    Implements functioning prim algo
    prim() now properly creates an minimum spanning tree and returns an
    array of parents of each node that isn't node 0. A short for loop was
    added in main() to print this list.
    findMinVert() finds the vertex in the vector Q that has the lowest key
    value and returns it.

--- 11Lab/awoersching.cpp
+++ 11Lab/awoersching.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 int* prim(vector<int> G[], int **weights);
+int findMinVert(vector<int> Q, int *keys);
 bool search(vector<int> Q, int v);
 
 int verts, edges;
@@ -16,12 +17,13 @@ int main(){
     cin >> verts;
     cin >> edges;
     int i, j, u, v, w, *parents;
-    int **weights = (int**)malloc(verts * sizeof(int*));
     vector<int> G[verts]; // Graph
-    for(i = 0; i < verts; i++)
+    int **weights = (int**)malloc(verts * sizeof(int*));
+    for(i = 0; i < verts; i++){
         weights[i] = (int*)malloc(verts * sizeof(int));
         for(j = 0; j < verts; j++)
             weights[i][j] = -1;
+    }
     for(i = 0; i < edges; i++){
         cin >> u >> v >> w;
         G[u].push_back(v);
@@ -30,35 +32,47 @@ int main(){
         weights[v][u] = w; // graph is undirected
     }
     parents = prim(G, weights);
+    for(i = 1; i < verts; i++) cout << parents[i] << endl;
     return 0;
 }
 
 int* prim(vector<int> G[], int **weights){
-    int i, u, v, min, num, *parents = (int*)malloc(verts * sizeof(int)), keys[verts];
+    int i, u, min, *parents = (int*)malloc(verts * sizeof(int)), *keys = (int*)malloc(verts * sizeof(int));
     vector<int> Q;
     for(i = 0; i < verts; i++){
         parents[i] = -1;
-        keys[i] = -1;
+        keys[i] = INT32_MAX;
         Q.push_back(i);
     }
     keys[0] = 0;
     while(Q.empty() == false){
-        u = Q[0];
-        Q.erase(Q.begin());
+        min = findMinVert(Q, keys);
+        u = Q[min];
+        Q.erase(Q.begin() + min);
         for(i = 0; i < G[u].size(); i++){
-            v = G[u][i];
-            if((search(Q, v)) && ((weights[u][v] < keys[v] || v == -1))){
-                parents[v] = u;
-                keys[v] = weights[u][v];
+            if((search(Q, G[u][i]) == true) && weights[u][G[u][i]] < keys[G[u][i]]){
+                parents[G[u][i]] = u;
+                keys[G[u][i]] = weights[u][G[u][i]];
             }
         }
     }
     return parents;
 }
 
+int findMinVert(vector<int> Q, int *keys){
+    int i, m = 0, num = keys[Q.front()];
+    for(i = 0; i < Q.size(); i++){
+        if(keys[Q[i]] < num){
+            num = keys[Q[i]];
+            m = i;
+        }
+    }
+    return m;
+}
+
 bool search(vector<int> Q, int v){
-    for(int i : Q)
-        if(v == i)
-            return true;
+    for(int i = 0; i < Q.size(); i++)
+        if(v == Q[i]) return true;
     return false;
 }
+
