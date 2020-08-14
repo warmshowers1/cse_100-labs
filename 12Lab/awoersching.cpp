d4a305a88cf8b75f597c248ef70e3992bc0beb1b
Alex Woersching <ajw006@gmail.com>
2020-08-14 01:34:27 -0700

    Creates first implementation of Bellman-Ford
    bell() is meant to implement the Bellman-Ford algorithm and print the
    shortest paths from the source (vertex 0) assuming there are no
    negative weight cycles. If there are, the function should simply print
    "FALSE" and end the cycle.
    Currently the issue with this code is that it always prints "FALSE" even
    when there no negative weight cycles.

--- 12Lab/awoersching.cpp
+++ 12Lab/awoersching.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+void bell(vector<int> vert[], int **weight);
+
 int v, e;
 
 int main(){
@@ -16,13 +18,54 @@ int main(){
     for(i = 0; i < v; i++){
         weight[i] = (int*)malloc(v * sizeof(int));
         for(j = 0; j < v; j++)
-            weight[i][j] = INT32_MAX;
+            weight[i][j] = INT32_MAX; // Closest number to infinity
     }
-    vector<int> verts[v];
+    vector<int> vert[v];
     for(i = 0; i < e; i++){
         cin >> u >> v >> w;
         weight[u][v] = w;
-        verts[u].push_back(v);
+        vert[u].push_back(v);
     }
+    bell(vert, weight);
     return 0;
 }
+
+void bell(vector<int> vert[], int **weight){
+    int i, j, k, dist[v], parent[v]; // Initializes distances of each vertex 
+    bool noCycle = true;
+    for(i = 0; i < v; i++){ // from source and parents of each vertex
+        dist[i] = INT32_MAX;
+        parent[i] = -1;
+    }
+    dist[0] = 0;
+    for(i = 0; i < (v-1); i++){
+        for(j = 0; j < v; j++){ // for each edge
+            for(k = 0; k < vert[j].size(); k++){
+                if(dist[k] > (dist[j] + weight[j][k])){ // Relax
+                    dist[k] = dist[j] + weight[j][k];
+                    parent[k] = j;
+                }
+            }
+        }
+    }
+    for(i = 0; i < v; i++){
+        for(j = 0; j < vert[i].size(); j++){
+            if(dist[j] > (dist[i] + weight[i][j])){
+                noCycle = false;
+                break;
+            }
+        }
+        if(noCycle == false) break;
+    }
+    if(noCycle) cout << "FALSE" << endl;
+    else{
+        cout << "TRUE" << endl;
+        for(i = 0; i < v; i++){
+            if(dist[i] == INT32_MAX)
+                cout << "INFINITY" << endl;
+            else
+                cout << dist[i] << endl;
+        }
+    }
+}
+
